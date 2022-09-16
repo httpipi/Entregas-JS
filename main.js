e545b6f54616bf3bb4900259fc4a5e526e75f174
httpipi <nicolas.a.rastelli@gmail.com>
2022-09-16 00:17:18 -0300

    se añadio formulario, se modifico el DOM para que al terminar de completar el formulario y se aprete la tecla ENTER se envie el formulario

--- main.js
+++ main.js
@@ -67,12 +67,10 @@ const carritoAdd = (codigo) => {
         itemAdd.cantidad = 1;
         carrito.push(itemAdd);
         listaCarrito();
-
     } else {
         carrito[codigoProducto].cantidad += 1;
         listaCarrito();
     }
-
 };
 //                      TOTAL CARRITO
 let total = document.getElementById('precioTotal')
@@ -99,9 +97,33 @@ const listaCarrito = () => {
         const totalFinal = document.createElement("div");
         totalFinal.className = "botoncompra";
         totalFinal.innerHTML = `
-        <button class="finCompra font btn-secondary btn-more2 text-decoration-none" id="fincompra" onClick="finalizarCompra()">Finalizar Compra</button>
+        <div class="form py-3">
+            <h3 class="font d-flex justify-content-center">Ingrese sus datos</h3>
+            <div class="row">
+                <div class="col">
+                    <input type="text" class="form-control" placeholder="Nombre Y Apellido" aria-label="Nombre Y Apellido" name="name" id="name">
+                </div>
+                <div class="col">
+                    <input type="text" class="form-control" placeholder="Telefono" aria-label="Telefono" name="phone" id="phone">
+                </div>
+                <div class="col">
+                    <input type="text" class="form-control" placeholder="Direccion" aria-label="Direccion" name="adress" id="adress">
+                </div>
+            </div>
+		</div>
+        <button class="finCompra font btn-secondary btn-more2 text-decoration-none d-flex justify-content-center" id="enviarInfo" onClick="(mostrarCliente)">Finalizar Compra</button>
         `
         canvasCarrito.appendChild(totalFinal);
+        let boton = document.getElementById("enviarInfo");
+        boton.addEventListener("click", cargarCliente);
+        //                  EVENTO TECLA ENTER
+        var input = document.getElementById("adress" , "name" , "phone");
+        input.addEventListener("keypress", function(event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            document.getElementById("enviarInfo").click();
+        }
+        });
     } else {
         canvasCarrito.classList.remove("carritoCanvas")
     }
@@ -111,4 +133,38 @@ const eliminarCodigo = (codigo) => {
     carrito.splice(codigo, 1);
     listaCarrito();
     total.innerText = carrito.reduce((acc, prod) => acc + (prod.precio * prod.cantidad), 0)
+}
+//                          FORMULARIO DE CLIENTE
+class Cliente {
+    constructor(nombre, telefono, direccion) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }
+}
+
+// capturar elementos
+function cargarCliente() {
+    let nombre = document.getElementById("name").value;
+    let telefono = document.getElementById("phone").value;
+    let direccion = document.getElementById("adress").value;
+    let cliente1 = new Cliente(nombre, telefono, direccion);
+    console.log(cliente1);
+    mostrarCliente(cliente1);
+}
+//eliminar elementos
+function mostrarCliente(cliente) {
+    let formulario = document.getElementById("cliente");
+    formulario.innerHTML = "";
+    //agregar elementos
+    let nuevoContenido = document.createElement("div");
+    nuevoContenido.innerHTML = `
+    <div class="alert alert-success p-3" role="alert">
+        <h4 class="alert-heading">¡${cliente.nombre}, Muchas Gracias!</h4>
+        <p>Sus datos fueron ingresados y su compra fue exitosa.</p>
+        <hr>
+        <p class="mb-0">Dentro de las proximas 48hs recibira su pedido en: ${cliente.direccion}.</p>
+    </div>`;
+    nuevoContenido.className = "info-cliente";
+    formulario.appendChild(nuevoContenido);
 }
